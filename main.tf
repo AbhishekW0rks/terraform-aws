8802259df60711b84fd11969050f8b477c1b6591
itsabhi06 <abhiishekabhi06@gmail.com>
2025-06-20 21:24:26 +0530

    Fix formatting with terraform fmt

--- main.tf
+++ main.tf
@@ -1,7 +1,18 @@
-module "web_ec2" {
-  source        = "./modules/ec2_instance"
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  name_tag      = var.name_tag
+resource "aws_instance" "web" {
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+  key_name      = "terraform-key"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Hello from Terraform</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "Terraform-Web"
+  }
 }
